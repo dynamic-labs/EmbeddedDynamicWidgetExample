a51132d04b80fad5d80e0e7fbdec9282e18d73fc
Matthew Foyle <mfoyle93@gmail.com>
2023-10-21 06:07:57 +0200

    feat: add the embedded widget

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  DynamicEmbeddedWidget,
+  DynamicContextProvider,
+} from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DynamicContextProvider
+        settings={{
+          environmentId: "4e598b41-f388-489b-a0b3-d24064b1d1ed",
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+      >
+        <div className="widget-container">
+          {/* background can be none, default or with-border */}
+          <DynamicEmbeddedWidget background="default" />
+        </div>
+      </DynamicContextProvider>
     </div>
   );
 }
